012e4dea96f13dff2294f827302b6f277e9b66e4
soneelee <shine.p715@gmail.com>
2024-09-29 15:55:32 +0900

    GROW-76: design. 해외 주식 통화 설정 버튼 UI 적용

--- frontend/src/features/sheet/ui/TableHeader.tsx
+++ frontend/src/features/sheet/ui/TableHeader.tsx
@@ -1,3 +1,5 @@
+import { SegmentedButtonGroup, SegmentedButton } from "@/shared";
+
 const TableHeader = ({ count, changeCurrency }) => {
   return (
     <div className="flex items-center justify-between pt-[20px]">
@@ -7,12 +9,20 @@ const TableHeader = ({ count, changeCurrency }) => {
         <span className="font-normal">건</span>
       </div>
 
-      <div className="flex items-center space-x-2">
-        <span className="color-gray-100 mr-[16px] text-[14px]">
+      <div className="flex items-center">
+        <span className="color-gray-100 mr-[16px] whitespace-nowrap text-[14px]">
           해외 주식 통화 설정
         </span>
-        <button onClick={() => changeCurrency(true)}>₩</button>
-        <button onClick={() => changeCurrency(false)}>$</button>
+        <div className="w-[56px] shrink-0">
+          <SegmentedButtonGroup>
+            <SegmentedButton onClick={() => changeCurrency(true)}>
+              ₩
+            </SegmentedButton>
+            <SegmentedButton onClick={() => changeCurrency(false)}>
+              $
+            </SegmentedButton>
+          </SegmentedButtonGroup>
+        </div>
       </div>
     </div>
   );
